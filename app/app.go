837fe7c2eb3fd41dc5b3797b82c98fccd02156c4
Lola-snt <lorrayne.work@gmail.com>
2024-04-14 15:40:51 -0300

    init

---
+++ app/app.go
@@ -0,0 +1,42 @@
+package app
+
+import (
+	"fmt"
+	"github.com/urfave/cli"
+	"log"
+	"net"
+)
+
+// Gerar vai retornar a aplicação de linha de comando pronta para ser executada
+func Gerar() *cli.App {
+	app := cli.NewApp()
+	app.Name = "Aplicação de Linha de comando"
+	app.Usage = "Busca IPs e Nomes de Servidor  na internet"
+
+	app.Commands = []cli.Command{
+
+		{
+			Name:  "ip",
+			Usage: "Busca Ips de endereços na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
+	return app
+}
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+}
